cfae73faf6eb02c3aeb92f05523be1c80935d7a9
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-06-04 06:23:36 +0200

    Update Apple.java

--- javaLessons/src/main/java/Lesson_5/Apple.java
+++ javaLessons/src/main/java/Lesson_5/Apple.java
@@ -1,15 +1,27 @@
-public class Apple <V extends Number> extends Fruit {
+public class Apple<V> extends Fruit {
 
     private V name;
+    private float weightA;
+    private int quantityA;
 
-    public Apple(V name) {
-        this.name=name;
-        }
+    public Apple(V name, int quantityA) {
+        this.name = name;
+        this.quantityA = quantityA;
+    }
 
+    public Apple(float weightA) {
+        this.weightA = weightA;
+    }
 
     public V getName() {
         return name;
     }
 
+    public float getWeightA() {
+        return 1.0f;
+    }
 
+    public int getQuantityA() {
+        return quantityA;
+    }
 }
