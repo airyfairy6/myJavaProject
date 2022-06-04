fba99227886fa9034ad60924cbdb1beb1462de11
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-06-04 06:28:13 +0200

    Update Orange.java

--- javaLessons/src/main/java/Lesson_5/Orange.java
+++ javaLessons/src/main/java/Lesson_5/Orange.java
@@ -1,15 +1,29 @@
-public class Orange<T extends Integer> extends Fruit{
+public class Orange<T extends String> extends Fruit {
 
     private T name;
+    private float weightO;
+    private int quantityO;
 
-        public Orange(T name) {
-            this.name=name;}
 
+    public Orange(T name, int quantityO) {
+        this.name = name;
+        this.quantityO = quantityO;
+    }
+
+
+    public Orange(float weightO) {
+        this.weightO = weightO;
+    }
+
+    public float getWeightO() {
+        return 1.5f;
+    }
 
     public T getName() {
         return name;
     }
 
-
+    public int getQuantityO() {
+        return quantityO;
+    }
 }
-
