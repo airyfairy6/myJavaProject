d95499465198a1df2ffabfc7bc16304b0d6515bd
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-06-04 06:24:42 +0200

    Update Box.java

--- javaLessons/src/main/java/Lesson_5/Box.java
+++ javaLessons/src/main/java/Lesson_5/Box.java
@@ -1,10 +1,44 @@
-public class Box <T extends Integer,V extends Number> {
-    private T nameO ;
-    private V nameA ;
+public class Box<T extends String, V> {
+    private T nameO;
+    private V nameA;
+    private int fullness;
+    private float weightOfBox;
+    private float weightPerEachFruit;
 
-public Box(){
+    public Box() {
+    }
 
-}
+    public int addFruitToBoxO(Orange<T> c) {
+        return fullness = fullness + c.getQuantityO();
+    }
+
+    public int addFruitToBoxA(Apple<V> c) {
+        return fullness = fullness + c.getQuantityA();
+    }
+
+
+    public float getWeightPerEachFruit() {
+        return weightPerEachFruit;
+    }
+
+
+    public int getFullness() {
+        return fullness;
+    }
+
+    public float getWeightPerEachFruitBO(Orange<T> c) {
+        return weightPerEachFruit = (float) (weightPerEachFruit + c.getWeightO());
+    }
+
+    public float getWeightPerEachFruitBA(Apple<V> c) {
+        return weightPerEachFruit = (float) (weightPerEachFruit + c.getWeightA());
+    }
+
+
+    public float getWeightOfBox() {
+        return weightOfBox;
+    }
+
     public T getNameO() {
         return nameO;
     }
@@ -20,5 +54,34 @@ public Box(){
     public Box(V nameA) {
         this.nameA = nameA;
     }
+
+    public float getWeight() {
+        return weightOfBox = (float) (getWeightOfBox() + (getFullness() * getWeightPerEachFruit()));
+
+    }
+
+
+    public boolean compare(Box<T, V> c) {
+        if (c.weightOfBox == weightOfBox) {
+            System.out.println("Коробки весят одинаково.");
+            return true;
+        } else {
+            System.out.println("Вес коробок отличается.");
+            return false;
+        }
+    }
+
+    public void passOrange(Box<T, V> c) {
+        int k = fullness;
+        fullness = 0;
+        c.fullness = k + c.fullness;
+    }
+
+    public void passApple(Box<T, V> c) {
+        int k = fullness;
+        fullness = 0;
+        c.fullness = k + c.fullness;
+    }
+
 
 }
