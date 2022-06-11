bc8b2f3878ccd81cae40608e399856ea674de0a1
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-06-11 04:38:43 +0200

    Update CreateCsv.java

--- Lesson_7/src/CreateCsv.java
+++ Lesson_7/src/CreateCsv.java
@@ -1,22 +1,57 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 public class CreateCsv {
     public static void main(String[] args) {
-        try { FileWriter writer = new FileWriter("names.txt");
-            BufferedWriter bwr = new BufferedWriter(writer);
-            bwr.write("James");
-            bwr.write("\n");
-            bwr.write("Hobert");
-            bwr.close();
-            System.out.println("succesfully written to a file");
+
+        AppData newData = new AppData();
+
+        BufferedWriter bw = null;
+        try {
+            File file = new File("C:\\Users\\Svetlana\\myJavaProject\\Lesson_7//GDP.txt");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            bw.write(Arrays.toString(newData.getHeader()));
+            bw.newLine();
+            for (int[] m : newData.getData())
+                bw.write(Arrays.toString(m));
+            bw.newLine();
+
+            System.out.println("File written Successfully");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         } catch (IOException ioe) {
             ioe.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (Exception ex) {
+                System.out.println("Error in closing the BufferedWriter" + ex);
+            }
         }
- AppData m= new AppData();
-        m.add(0,"Value");
+        File file = new File("C:\\Users\\Svetlana\\myJavaProject\\Lesson_7//GDP.txt");
+
+        String line;
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader(file))) {
+            while ((line = br.readLine()) != null) {
+                String[] k = line.split(";");
+
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
 }
+
 
 
