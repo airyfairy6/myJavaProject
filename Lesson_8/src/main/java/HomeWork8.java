e47e1e6015619c1ed9096b80952f0285b1723637
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-06-14 01:43:50 +0200

    Update HomeWork8.java

--- Lesson_8/src/main/java/HomeWork8.java
+++ Lesson_8/src/main/java/HomeWork8.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 public class HomeWork8 {
     public static void main(String[] args) {
-            HashMap <Integer,String>  book = new HashMap<Integer,String>();
-            book.put(111,"Ivanov");
-            book.put(222, "Petrov");
-            book.put(333, "Ivanov");
-            book.put(444, "Sidorov");
+        //ЗАДАНИЕ 2
+        PhoneBook book = new PhoneBook();
+        System.out.println("The PhoneBook consists of the following records: " + '\n' + book.create());
+        System.out.println();
+        book.add(99999, "Vasiliev");
+        System.out.println();
+        book.get("Petrov");
 
-        for (HashMap.Entry<Integer, String> o : book.entrySet()) {
-            System.out.println(o.getValue()+ ": " +o.getKey());
-        }
 
         System.out.println();
- //ЗАДАНИЕ 1
+        //ЗАДАНИЕ 1
         String arr[] = new String[]{"January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November",
                 "December", "January", "March", "March"};
